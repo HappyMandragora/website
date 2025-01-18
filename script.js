4d479b40dbaa86abdbcc0852b68bf92e2a440621
HappyMandragora <90543449+HappyMandragora@users.noreply.github.com>
2025-01-18 17:27:25 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const carouselSlide = document.querySelector('.carousel-slide');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let counter = 0;
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+function updateCarousel() {
+    const slideWidth = carouselSlide.children[0].clientWidth;
+    carouselSlide.style.transform = `translateX(-${counter * slideWidth}px)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    counter = (counter + 1) % carouselSlide.children.length;
+    updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+    counter = (counter - 1 + carouselSlide.children.length) % carouselSlide.children.length;
+    updateCarousel();
+});
+
+
+
+setInterval(() => {
+    counter = (counter + 1) % carouselSlide.children.length;
+    updateCarousel();
+}, 5000);
